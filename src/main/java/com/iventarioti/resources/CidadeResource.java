a5356997e3aa662c83262ff4bd1c2c07744caf48
Sadraque Nunes <sadraquenunesmartiniano@gmail.com>
2021-02-07 20:11:49 -0300

    Correcao de exclusao de id nulo - Endpoint cidades

--- src/main/java/com/iventarioti/resources/CidadeResource.java
+++ src/main/java/com/iventarioti/resources/CidadeResource.java
@@ -3,6 +3,7 @@ package com.iventarioti.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,9 +37,14 @@ public class CidadeResource {
 	
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<?> deletar(@PathVariable Integer id) {
-		cidadeService.deletar(id);
-		
-		return ResponseEntity.ok().build();
+		try {
+			cidadeService.deletar(id);
+
+			return ResponseEntity.ok().build();
+		} catch (EmptyResultDataAccessException e) {
+
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 }
